98a0f3e7f5f6f19a83ed244418df44014e26f7ec
Christyano Romano <christyano.r@gmail.com>
2018-07-25 23:08:56 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/crtecnologia/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.crtecnologia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crtecnologia.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
